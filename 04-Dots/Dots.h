5f78c119b3d642be4f9851e0267abf616ce56daa
Andrew Dent <dent.ace@gmail.com>
2022-09-06 20:55:26 +0100

    Add `Dots.h` cpp file
    First check-in.

---
+++ 04-Dots/Dots.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <stdint.h> // For uint8_t
+#include <avr/pgmspace.h> // For PROGMEM
+
+namespace patterns {
+
+    constexpr uint8_t PROGMEM confetti[] {
+        8, 8,  // 8x8 px image
+        0x04,  // ▓▓▓▓▓░▓▓
+        0x40,  // ▓░▓▓▓▓▓▓
+        0x01,  // ▓▓▓▓▓▓▓░
+        0x08,  // ▓▓▓▓░▓▓▓
+        0x20,  // ▓▓░▓▓▓▓▓
+        0x02,  // ▓▓▓▓▓▓░▓
+        0x80,  // ░▓▓▓▓▓▓▓
+        0x10   // ▓▓▓░▓▓▓▓
+    };
+    // Magic: "\4@\1\b \2\200\20"[i%8]
+
+    constexpr uint8_t PROGMEM confettiLarge[] {
+        8, 8,  // 8x8 px image
+        0x0C,  // ▓▓▓▓░░▓▓
+        0x6C,  // ▓░░▓░░▓▓
+        0x63,  // ▓░░▓▓▓░░
+        0x03,  // ▓▓▓▓▓▓░░
+        0x30,  // ▓▓░░▓▓▓▓
+        0x36,  // ▓▓░░▓░░▓
+        0xC6,  // ░░▓▓▓░░▓
+        0xC0   // ░░▓▓▓▓▓▓
+    };
+    // Magic: "\flc\00306\306\300"[i%8]
+
+}
+
+
+
+#if 0
+
+namespace patternsHorizontal {
+
+    constexpr uint8_t PROGMEM confetti[] {
+        8, 8,  // 8x8 px image
+        0x20,  // ▓▓░▓▓▓▓▓
+        0x04,  // ▓▓▓▓▓░▓▓
+        0x80,  // ░▓▓▓▓▓▓▓
+        0x10,  // ▓▓▓░▓▓▓▓
+        0x01,  // ▓▓▓▓▓▓▓░
+        0x08,  // ▓▓▓▓░▓▓▓
+        0x40,  // ▓░▓▓▓▓▓▓
+        0x02,  // ▓▓▓▓▓▓░▓
+    };
+
+    constexpr uint8_t PROGMEM confettiLarge[] {
+        8, 8,  // 8x8 px image
+        0x30,  // ▓▓░░▓▓▓▓
+        0x36,  // ▓▓░░▓░░▓
+        0xC6,  // ░░▓▓▓░░▓
+        0xC0,  // ░░▓▓▓▓▓▓
+        0x0C,  // ▓▓▓▓░░▓▓
+        0x6C,  // ▓░░▓░░▓▓
+        0x63,  // ▓░░▓▓▓░░
+        0x03   // ▓▓▓▓▓▓░░
+    };
+}
+
+#endif
