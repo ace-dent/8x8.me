247ecc068bd10f233f2c054b2617f0a6e1db54bc
Andrew Dent <dent.ace@gmail.com>
2022-09-20 12:14:42 +0100

    Improve `createimages.sh` script
    - Determine the image ‘group’ automatically from the containing folder.
    - Add metadata to the canonical png image.
    - Add the public domain/ CC0 license details.

--- utils/createimages.sh
+++ utils/createimages.sh
@@ -7,16 +7,28 @@ then
   exit
 fi
 
+
+COPYRIGHT="This work is dedicated to the Public Domain by ACED, licensed under CC0."
+LICENSE="https://creativecommons.org/publicdomain/zero/1.0/"
+
+
 while (( "$#" )); do
-  NAME=`basename -s .png "$1"` # Get the filename
+  # Get an image name and group from the filename
+  NAME=`basename -s .png "$1"`
+  GROUP=`basename "${1%/*/*}" | cut -f 2 -d "-"`
   
-  # Create a Portable Bit Map (PBM) image file
+  # Add metadata to the original png file
+  exiftool -q -overwrite_original -fast1 -Title="$NAME - $GROUP - 8x8.me fill pattern." -Copyright="$COPYRIGHT $LICENSE" "$1"
+
+  # Create the Portable Bit Map (PBM) file
   PBMFILE="${1%/*/*}/pbm/$NAME.pbm"
   magick "$1" -depth 1 -compress None "$PBMFILE"
-  exiftool -overwrite_original -Comment="$NAME - GROUP - 8x8.me fill pattern." "$PBMFILE"
-  
+  exiftool -q -overwrite_original -fast5 -Comment="$NAME - $GROUP - 8x8.me fill pattern." "$PBMFILE"
+  printf "\n# $COPYRIGHT\n# $LICENSE" >> "$PBMFILE"
+
   # Create the preview image
-  magick -size 32x16 tile:"$1" -sample 200% "${1%/*/*/*}/previews/$NAME.png"
-  
+  PREVIEW="${1%/*/*/*}/previews/$NAME.png"
+  magick -size 32x16 tile:"$1" -sample 200% "$PREVIEW"
+
   shift
 done
