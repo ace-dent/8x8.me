f87ee5ebb705916195c0cdfcecaf7463bf7f40f2
Andrew Dent <dent.ace@gmail.com>
2022-11-14 20:49:15 +0000

    Update for horizontal line patterns (Pt 1)
    - Update source `.xml` artwork (in progress).
    - Add missing image metadata for `HorizontalDense` pattern.
    - Add code for first half of the horizontal patterns.

--- 04-Lines/Lines.h
+++ 04-Lines/Lines.h
@@ -8,17 +8,169 @@
 
 
 namespace patterns {
-    
-    
-    
+
+    constexpr uint8_t PROGMEM horizontalDenseLineDash[] {
+        8, 8,  // 8x8 px image
+        0x55,  // ▓░▓░▓░▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x11,  // ▓▓▓░▓▓▓░
+        0x11,  // ▓▓▓░▓▓▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x11,  // ▓▓▓░▓▓▓░
+        0x11,  // ▓▓▓░▓▓▓░
+    };
+    // Magic: "UU\21\21"[i%4]
+    // GAMBY: 0x5511
+
+    constexpr uint8_t PROGMEM horizontalDenseLineDot[] {
+        8, 8,  // 8x8 px image
+        0x55,  // ▓░▓░▓░▓░
+        0x11,  // ▓▓▓░▓▓▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x11,  // ▓▓▓░▓▓▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x11,  // ▓▓▓░▓▓▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x11,  // ▓▓▓░▓▓▓░
+    };
+    // Magic: i&1?85:17
+    // GAMBY: 0x5151
+
+    constexpr uint8_t PROGMEM horizontalDense[] {
+        8, 8,  // 8x8 px image
+        0x55,  // ▓░▓░▓░▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x55,  // ▓░▓░▓░▓░
+        0x55,  // ▓░▓░▓░▓░
+    };
+    // Magic: 85
+    // GAMBY: 0x5555
+
+    constexpr uint8_t PROGMEM horizontal[] {
+        8, 8,  // 8x8 px image
+        0x44,  // ▓░▓▓▓░▓▓
+        0x44,  // ▓░▓▓▓░▓▓
+        0x44,  // ▓░▓▓▓░▓▓
+        0x44,  // ▓░▓▓▓░▓▓
+        0x44,  // ▓░▓▓▓░▓▓
+        0x44,  // ▓░▓▓▓░▓▓
+        0x44,  // ▓░▓▓▓░▓▓
+        0x44,  // ▓░▓▓▓░▓▓
+    };
+    // Magic: 68
+    // GAMBY: 0x4444
+
+    constexpr uint8_t PROGMEM horizontalMedium[] {
+        8, 8,  // 8x8 px image
+        0x66,  // ▓░░▓▓░░▓
+        0x66,  // ▓░░▓▓░░▓
+        0x66,  // ▓░░▓▓░░▓
+        0x66,  // ▓░░▓▓░░▓
+        0x66,  // ▓░░▓▓░░▓
+        0x66,  // ▓░░▓▓░░▓
+        0x66,  // ▓░░▓▓░░▓
+        0x66,  // ▓░░▓▓░░▓
+    };
+    // Magic: 102
+    // GAMBY: 0x6666
+
+    constexpr uint8_t PROGMEM horizontalBold[] {
+        8, 8,  // 8x8 px image
+        0x77,  // ▓░░░▓░░░
+        0x77,  // ▓░░░▓░░░
+        0x77,  // ▓░░░▓░░░
+        0x77,  // ▓░░░▓░░░
+        0x77,  // ▓░░░▓░░░
+        0x77,  // ▓░░░▓░░░
+        0x77,  // ▓░░░▓░░░
+        0x77,  // ▓░░░▓░░░
+    };
+    // Magic: 119
+    // GAMBY: 0x7777
+
 }
 
 
 
 #if 0
 namespace patternsHorizontal {
-    
-    
-    
+
+    constexpr uint8_t PROGMEM horizontalDenseLineDash[] {
+        8, 8,  // 8x8 px image
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xCC,  // ░░▓▓░░▓▓
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xCC,  // ░░▓▓░░▓▓
+        0x00,  // ▓▓▓▓▓▓▓▓
+    };
+
+    constexpr uint8_t PROGMEM horizontalDenseLineDot[] {
+        8, 8,  // 8x8 px image
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xAA,  // ░▓░▓░▓░▓
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xAA,  // ░▓░▓░▓░▓
+        0x00,  // ▓▓▓▓▓▓▓▓
+    };
+
+    constexpr uint8_t PROGMEM horizontalDense[] {
+        8, 8,  // 8x8 px image
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+    };
+
+    constexpr uint8_t PROGMEM horizontal[] {
+        8, 8,  // 8x8 px image
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+    };
+
+    constexpr uint8_t PROGMEM horizontalMedium[] {
+        8, 8,  // 8x8 px image
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xFF,  // ░░░░░░░░
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xFF,  // ░░░░░░░░
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+    };
+
+    constexpr uint8_t PROGMEM horizontalBold[] {
+        8, 8,  // 8x8 px image
+        0xFF,  // ░░░░░░░░
+        0xFF,  // ░░░░░░░░
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+        0xFF,  // ░░░░░░░░
+        0xFF,  // ░░░░░░░░
+        0xFF,  // ░░░░░░░░
+        0x00,  // ▓▓▓▓▓▓▓▓
+    };
+
 }
 #endif
--- 04-Lines/Lines.thumby.py
+++ 04-Lines/Lines.thumby.py
@@ -2,3 +2,81 @@
 # This work is dedicated to the Public Domain by ACED, licensed under CC0
 # https://creativecommons.org/publicdomain/zero/1.0/
 
+
+horizontalDenseLineDash = bytearray([
+    # BITMAP: width: 8, height: 8, [85,85,17,17,85,85,17,17]
+    0x55,  # ▓░▓░▓░▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x11,  # ▓▓▓░▓▓▓░
+    0x11,  # ▓▓▓░▓▓▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x11,  # ▓▓▓░▓▓▓░
+    0x11,  # ▓▓▓░▓▓▓░
+])
+# horizontalDenseLineDashSprite = thumby.Sprite(8, 8, horizontalDenseLineDash)
+
+horizontalDenseLineDot = bytearray([
+    # BITMAP: width: 8, height: 8, [85,17,85,17,85,17,85,17]
+    0x55,  # ▓░▓░▓░▓░
+    0x11,  # ▓▓▓░▓▓▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x11,  # ▓▓▓░▓▓▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x11,  # ▓▓▓░▓▓▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x11,  # ▓▓▓░▓▓▓░
+])
+# horizontalDenseLineDotSprite = thumby.Sprite(8, 8, horizontalDenseLineDot)
+
+horizontalDense = bytearray([
+    # BITMAP: width: 8, height: 8, [85,85,85,85,85,85,85,85]
+    0x55,  # ▓░▓░▓░▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x55,  # ▓░▓░▓░▓░
+    0x55,  # ▓░▓░▓░▓░
+])
+# horizontalDenseSprite = thumby.Sprite(8, 8, horizontalDense)
+
+horizontal = bytearray([
+    # BITMAP: width: 8, height: 8, [68,68,68,68,68,68,68,68]
+    0x44,  # ▓░▓▓▓░▓▓
+    0x44,  # ▓░▓▓▓░▓▓
+    0x44,  # ▓░▓▓▓░▓▓
+    0x44,  # ▓░▓▓▓░▓▓
+    0x44,  # ▓░▓▓▓░▓▓
+    0x44,  # ▓░▓▓▓░▓▓
+    0x44,  # ▓░▓▓▓░▓▓
+    0x44,  # ▓░▓▓▓░▓▓
+])
+# horizontalSprite = thumby.Sprite(8, 8, horizontal)
+
+horizontalMedium = bytearray([
+    # BITMAP: width: 8, height: 8, [102,102,102,102,102,102,102,102]
+    0x66,  # ▓░░▓▓░░▓
+    0x66,  # ▓░░▓▓░░▓
+    0x66,  # ▓░░▓▓░░▓
+    0x66,  # ▓░░▓▓░░▓
+    0x66,  # ▓░░▓▓░░▓
+    0x66,  # ▓░░▓▓░░▓
+    0x66,  # ▓░░▓▓░░▓
+    0x66,  # ▓░░▓▓░░▓
+])
+# horizontalMediumSprite = thumby.Sprite(8, 8, horizontalMedium)
+
+horizontalBold = bytearray([
+    # BITMAP: width: 8, height: 8, [119,119,119,119,119,119,119,119]
+    0x77,  # ▓░░░▓░░░
+    0x77,  # ▓░░░▓░░░
+    0x77,  # ▓░░░▓░░░
+    0x77,  # ▓░░░▓░░░
+    0x77,  # ▓░░░▓░░░
+    0x77,  # ▓░░░▓░░░
+    0x77,  # ▓░░░▓░░░
+    0x77,  # ▓░░░▓░░░
+])
+# horizontalBoldSprite = thumby.Sprite(8, 8, horizontalBold)
