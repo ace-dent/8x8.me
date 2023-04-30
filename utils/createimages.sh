51769ea602b44c5b1a24ecb0932b9d56eda5e9e0
Andrew Dent <dent.ace@gmail.com>
2023-04-30 19:21:16 +0100

    Improve `createimages.sh` script
    - Change variable names to `snake_case` style.
    - Use `readonly` for script constants.
    - Prefer `$()` over backtick syntax.
    - Combine `tr` substitutions for binary digits.
    - More consistent use of `printf` when writing to file.
    - Try to improve readability.

--- utils/createimages.sh
+++ utils/createimages.sh
@@ -1,117 +1,132 @@
 #!/bin/bash
 
-# WARNING: Hacked together; may do terrible things!
+# WARNING: Not safe for public use! Created for the author's benefit.
+# Assumes filenames and directory structure follows the project standard.
 
 
 # Minimal check for input file(s)
 if [ -z "$1" ]; then
-  echo "Missing filename for png image to process."
+  echo "Missing filename. Provide at least one png image to process."
   exit
 fi
 
-# TODO: Check for executables (ImageMagick and ExifTool)
+# TODO: Check for external executables (ImageMagick and ExifTool)
 
 
 # Directory for utilities
-UTILDIR="$(dirname "${BASH_SOURCE[0]}")"
+readonly util_dir="$(dirname "${BASH_SOURCE[0]}")"
+
+# Standard metadata for images
+readonly project='8x8.me fill pattern.'
+readonly copyright='This work is dedicated to the Public Domain by ACED, licensed under CC0.'
+readonly license='https://creativecommons.org/publicdomain/zero/1.0/'
 
 
-COPYRIGHT="This work is dedicated to the Public Domain by ACED, licensed under CC0."
-LICENSE="https://creativecommons.org/publicdomain/zero/1.0/"
-
-IMAGECOUNT=0
+img_counter=0
 while (( "$#" )); do
   
-  ((IMAGECOUNT=IMAGECOUNT+1))
+  ((img_counter=img_counter+1))
   
-  # Get an image name and group from the filename
-  NAME=`basename -s .png "$1"`
-  GROUP=`basename "${1%/*/*}" | cut -f 2 -d "-"`
-  # Format variations for name (lowercase, camelCase)
-  name=$( echo "${NAME}" | tr [:upper:] [:lower:] )
-  nAME=$( echo "${NAME:0:1}" | tr [:upper:] [:lower:] )"${NAME:1}"
+  # Get the image name and group from the filename
+  img_name="$( basename -s .png "$1" )"
+  img_root="${1%/*/*}" # Base directory for the pattern
+  img_group="$( basename "${img_root}" | cut -f 2 -d "-" )"
+  # Format variations for image name
+  name_lowercase="$( echo "${img_name}" | tr '[:upper:]' '[:lower:]' )"
+  name_camelcase="${name_lowercase:0:1}${img_name:1}" # Lower case first letter and append remaining characters
+  name_camelcase="$( echo -n "${name_camelcase}" | tr -cs '[:alnum:]' '_')" # Replace non-alphanumeric characters with `_` for code compatibility
 
 
   # Add metadata to the original png file
-  exiftool -q -overwrite_original -fast1 -Title="$NAME - $GROUP - 8x8.me fill pattern." -Copyright="$COPYRIGHT $LICENSE" "$1"
+  exiftool -q -overwrite_original -fast1 \
+    -Title="${img_name} - ${img_group} - ${project}" \
+    -Copyright="${copyright} ${license}" "$1"
 
   # Create the Portable Bit Map (PBM) file
-  PBMFILE="${1%/*/*}/pbm/$NAME.pbm"
-  magick "$1" -depth 1 -compress None "$PBMFILE"
-  exiftool -q -overwrite_original -fast5 -Comment="$NAME - $GROUP - 8x8.me fill pattern." "$PBMFILE"
-  printf "\n# $COPYRIGHT\n# $LICENSE" >> "$PBMFILE"
+  pbm_file="${img_root}/pbm/${img_name}.pbm"
+  magick "$1" -depth 1 -compress None "${pbm_file}"
+  exiftool -q -overwrite_original -fast5 \
+    -Comment="${img_name} - ${img_group} - ${project}" "${pbm_file}" # Primary metadata (single text line in header)
+  printf '\n# %s' "${copyright}" "${license}" >> "${pbm_file}" # Extra metadata appended to the plain text file
 
   # Create the preview image
-  PREVIEW="${1%/*/*/*}/previews/$NAME.png"
-  magick -size 32x16 tile:"$1" -sample 200% "$PREVIEW"
+  preview_file="${img_root%/*}/previews/${img_name}.png"
+  magick -size 32x16 tile:"$1" -sample 200% "${preview_file}" # Tile the image 4x2 then scale up 2x
   
   
   # Create Horizontal and Vertical temporary files, in plain text binary and hexadecimal
-  HBIN="${1%/*/*}/$NAME.h-bin.txt"
-  VBIN="${1%/*/*}/$NAME.v-bin.txt"
-  HHEX="${1%/*/*}/$NAME.h-hex.txt"
-  VHEX="${1%/*/*}/$NAME.v-hex.txt"
-  magick "$1" -depth 1 +negate -compress None PBM:- | tail -n +3 | tr -d " " > "$HBIN"
-  magick "$1" -depth 1 +negate -rotate 90 -compress None PBM:- | tail -n +3 | tr -d " " > "$VBIN"
+  HBIN="${img_root}/$img_name.h-bin.txt"
+  VBIN="${img_root}/$img_name.v-bin.txt"
+  HHEX="${img_root}/$img_name.h-hex.txt"
+  VHEX="${img_root}/$img_name.v-hex.txt"
+  magick "$1" -depth 1 +negate -compress None PBM:- \
+    | tail -n +3 \
+    | tr -d " " > "$HBIN"
+  magick "$1" -depth 1 +negate -rotate 90 -compress None PBM:- \
+    | tail -n +3 \
+    | tr -d " " > "$VBIN"
   echo -n "" > "$HHEX"
   echo -n "" > "$VHEX"
-  LINE=1
-  while [ $LINE -le 8 ]; do
-    printf '    0x%02X,  # ' "$((2#`sed -n "$LINE"p "$HBIN"`))" >> "$HHEX"
-    sed -n "$LINE"p "$HBIN" | tr "0" "▓" | tr "1" "░" >> "$HHEX"
-    printf '    0x%02X,  # ' "$((2#`sed -n "$LINE"p "$VBIN"`))" >> "$VHEX"
-    sed -n "$LINE"p "$VBIN" | tr "0" "▓" | tr "1" "░" >> "$VHEX"
-    ((LINE=LINE+1))
+  line=1; binary=""
+  # Write files with hexadecimal values and comment lines
+  while [ ${line} -le 8 ]; do
+    binary="$( sed -n ${line}p "$HBIN" )" # Horizontal
+    printf '    0x%02X' "$((2#${binary}))" >> "$HHEX"
+    printf '  # %s\n' "${binary}" | tr '01' '▓░' >> "$HHEX"
+    binary="$( sed -n ${line}p "$VBIN" )" # Vertical
+    printf '    0x%02X' "$((2#${binary}))" >> "$VHEX"
+    printf '  # %s\n' "${binary}" | tr '01' '▓░' >> "$VHEX"
+    ((line=line+1))
   done
-  PATTERNWIDTH=8
+  pattern_width=8
   if [ "`head -n 4 "$VBIN"`" = "`tail -n 4 "$VBIN"`" ]; then
-    PATTERNWIDTH=4
+    pattern_width=4
     if [ "`head -n 2 "$VBIN"`" = "`head -n 4 "$VBIN" | tail -n 2`" ]; then
-      PATTERNWIDTH=2
+      pattern_width=2
       if [ "`head -n 1 "$VBIN"`" = "`head -n 2 "$VBIN" | tail -n 1`" ]; then
-        PATTERNWIDTH=1
+        pattern_width=1
       fi
     fi
   fi
-  PATTERNHEIGHT=8
+  pattern_height=8
   if [ "`head -n 4 "$HBIN"`" = "`tail -n 4 "$HBIN"`" ]; then
-    PATTERNHEIGHT=4
+    pattern_height=4
   fi
-  echo "$NAME - Pattern width $PATTERNWIDTH px x height $PATTERNHEIGHT px."
+  echo "$img_name - Pattern width $pattern_width px x height $pattern_height px."
 
 
   # Create Bitsy tile data
-  WIPFILE="${1%/*/*}/$GROUP.bitsy.WIP.txt"
-  printf "\nTIL $IMAGECOUNT\n" >> "$WIPFILE"
-  # TODO: Change decimal count to base36?
-  cat "$HBIN" >> "$WIPFILE"
-  printf "NAME $name\n" >> "$WIPFILE"
+  bitsy_file="${img_root}/${img_group}.bitsy.WIP.txt"
+  printf '\nTIL %d\n' "${img_counter}" >> "${bitsy_file}" # Decimal TIL number would ideally be base36 for Bitsy
+  cat "$HBIN" >> "${bitsy_file}"
+  printf 'NAME %s\n' "${name_lowercase}" >> "${bitsy_file}"
 
 
   # Create cpp (Arduboy) code
-  WIPFILE="${1%/*/*}/$GROUP.h.WIP.txt"
-  HWIPFILE="${1%/*/*}/$GROUP.h.WIP2.txt"
+  cpp_file="${img_root}/${img_group}.h.WIP.txt" # Vertical data
+  cpp_hori_file="${img_root}/${img_group}.h.WIP2.txt" # Horizontal data
   # Vertical format data (sprite standard)
-  printf "\nconstexpr uint8_t PROGMEM $nAME[] {\n    8, 8,  // 8x8 px image\n" >> "$WIPFILE"
-  cat "$VHEX" | sed 's/#/\/\//' >> "$WIPFILE"
+  printf '\nconstexpr uint8_t PROGMEM %s[] {\n' "${name_camelcase}" >> "$cpp_file"
+  printf '    8, 8,  // 8x8 px image\n' >> "${cpp_file}"
+  sed 's|#|//|g' "$VHEX" >> "${cpp_file}" # Change `#` to `//` for comments
   # Alternative 'magic' representation
-  printf "};\n// Magic: " >> "$WIPFILE"
-  if [ $PATTERNWIDTH -eq 1 ]; then
-    printf '%i\n' "$((2#`sed -n 1p "$VBIN"`))" >> "$WIPFILE"
-  elif [ $PATTERNWIDTH -eq 2 ]; then
+  printf '};\n// Magic: ' >> "${cpp_file}"
+  if [ ${pattern_width} -eq 1 ]; then
+    printf '%i\n' "$((2#`sed -n 1p "$VBIN"`))" >> "${cpp_file}"
+  elif [ ${pattern_width} -eq 2 ]; then
     # TODO: Catch edge cases where string encoding is smaller "Az"[i%2]
     # e.g. bayerDither04 `"U"[i%2]` vs `i&1?85:0`
-    printf 'i&1?%i' "$((2#`sed -n 1p "$VBIN"`))" >> "$WIPFILE"
-    printf ':%i\n' "$((2#`sed -n 2p "$VBIN"`))" >> "$WIPFILE"
+    printf 'i&1?%i' "$((2#`sed -n 1p "$VBIN"`))" >> "${cpp_file}"
+    printf ':%i\n' "$((2#`sed -n 2p "$VBIN"`))" >> "${cpp_file}"
   else
     # Encode 'magic' as a string... hold tight...
     # First pass: scan byte values to estimate if inverted form will be more compact
-    COLUMN=$PATTERNWIDTH; VALUE=""; INVERT=0
-    while [ $COLUMN -gt 0 ]; do
+    column=$pattern_width; VALUE=""; INVERT=0
+    while [ $column -gt 0 ]; do
       # Find the decimal value for the current column (byte)
-      VALUE=$(printf '%u' "$((2#`sed -n "$COLUMN"p "$VBIN"`))")
+      VALUE=$(printf '%u' "$((2#`sed -n ${column}p "$VBIN"`))")
       if [ $VALUE -le 13 ]; then
-        if [ $VALUE -eq 0 ] && [ $COLUMN = $PATTERNWIDTH ]; then
+        if [ $VALUE -eq 0 ] && [ $column = $pattern_width ]; then
           ((INVERT=INVERT+4)) # cpp strings are null terminated
         else
           ((INVERT=INVERT+2))
@@ -125,32 +140,32 @@ while (( "$#" )); do
       elif [ $VALUE -le 241 ]; then
         ((INVERT=INVERT-1))
       else
-        if [ $VALUE -eq 255 ] && [ $COLUMN = $PATTERNWIDTH ]; then
+        if [ $VALUE -eq 255 ] && [ $column = $pattern_width ]; then
           ((INVERT=INVERT-4)) # cpp strings are null terminated
         else
           ((INVERT=INVERT-2))
         fi
       fi
-      ((COLUMN=COLUMN-1))
+      ((column=column-1))
     done
     # Second pass: Encode the string
     # Process the columns (bytes) in reverse order
-    COLUMN=$PATTERNWIDTH; VALUE=""; DIGIT=0
+    column=$pattern_width; VALUE=""; digit_present=0
     # We store the end of the string first
-    STRING="\"[i%$PATTERNWIDTH]"
-    while [ $COLUMN -gt 0 ]; do
+    STRING="\"[i%$pattern_width]"
+    while [ $column -gt 0 ]; do
       # Find the octal value for the current column (byte)
-      VALUE=$(printf '%o' "$((2#`sed -n "$COLUMN"p "$VBIN"`))")
+      VALUE=$(printf '%o' "$((2#`sed -n ${column}p "$VBIN"`))")
       if [ $INVERT -lt -1 ]; then
         ((VALUE=377-VALUE))
       fi
       # Process the octal value, to handle special cases for encoding
       case $VALUE in
         [0-6])
-          if [ $VALUE -eq 0 ] && [ $COLUMN = $PATTERNWIDTH ]; then
+          if [ $VALUE -eq 0 ] && [ $column = $pattern_width ]; then
             # cpp strings are always null terminated
             VALUE=""
-          elif [ $DIGIT -eq 1 ]; then
+          elif [ ${digit_present} -eq 1 ]; then
             # Add leading zeroes to make octal value unambiguous 
             VALUE="\\00$VALUE"
           else
@@ -180,7 +195,7 @@ while (( "$#" )); do
           VALUE="\\r"
         ;;
         1[6-7]|2[0-7]|3[0-2])
-          if [ $DIGIT -eq 1 ]; then
+          if [ ${digit_present} -eq 1 ]; then
             # Add leading zero to make octal value unambiguous 
             VALUE="\\0$VALUE"
           else
@@ -191,7 +206,7 @@ while (( "$#" )); do
           VALUE="\\e"
         ;;
         3[4-7])
-          if [ $DIGIT -eq 1 ]; then
+          if [ ${digit_present} -eq 1 ]; then
             VALUE="\\0$VALUE"
           else
             VALUE="\\$VALUE"
@@ -221,115 +236,115 @@ while (( "$#" )); do
           VALUE="\\""$VALUE"
         ;;
       esac
-      # Check if the character output is a valid octal digit (0–7)
+      # Check if the character output is a valid octal digit (0-7)
       # This may affect encoding of the next (preceding) octal value
       case $VALUE in
         [0-7])
-          DIGIT=1
+          digit_present=1
         ;;
         *)
-          DIGIT=0
+          digit_present=0
         ;;
       esac
       STRING="$VALUE""$STRING"
-      ((COLUMN=COLUMN-1))
+      ((column=column-1))
     done
     STRING="\"""$STRING"
     if [ $INVERT -lt -1 ]; then
       STRING="~""$STRING"
     fi
-    echo "$STRING" >> "$WIPFILE"
+    printf '%s\n' "$STRING" >> "${cpp_file}"
   fi
   # Bonus 4x4px GAMBY data
-  if [ $PATTERNWIDTH -le 4 ] && [ $PATTERNHEIGHT -le 4 ]; then
-    printf "// GAMBY: 0x" >> "$WIPFILE"
-    COLUMN=1
-    while [ $COLUMN -le 4 ]; do
-      sed -n "$COLUMN"p "$VHEX" | head -c 7 | tail -c 1 >> "$WIPFILE"
-      ((COLUMN=COLUMN+1))
+  if [ $pattern_width -le 4 ] && [ $pattern_height -le 4 ]; then
+    printf '// GAMBY: 0x' >> "${cpp_file}"
+    column=1
+    while [ $column -le 4 ]; do
+      sed -n ${column}p "$VHEX" | head -c 7 | tail -c 1 >> "${cpp_file}"
+      ((column=column+1))
     done
-    printf "\n" >> "$WIPFILE"
+    printf '\n' >> "${cpp_file}"
   fi
-  # Additional horizontal format (WIP2 file)
-  printf "\nconstexpr uint8_t PROGMEM $nAME[] {\n    8, 8,  // 8x8 px image\n" >> "$HWIPFILE"
-  cat "$HHEX" | sed 's/#/\/\//' >> "$HWIPFILE"
-  printf "};\n" >> "$HWIPFILE"
+  # Additional horizontal format (seperate file)
+  printf '\nconstexpr uint8_t PROGMEM %s[] {\n' "${name_camelcase}" >> "${cpp_hori_file}"
+  printf '    8, 8,  // 8x8 px image\n' >> "${cpp_hori_file}"
+  sed 's|#|//|g' "$HHEX" >> "${cpp_hori_file}" # Change `#` to `//` for comments
+  printf '};\n' >> "${cpp_hori_file}"
 
 
   # Create PICO-8 code
-  WIPFILE="${1%/*/*}/$GROUP.p8.lua.WIP.txt"
+  p8_file="${img_root}/${img_group}.p8.lua.WIP.txt"
   # Produce data as a custom font
-  ((OFFSET=96+IMAGECOUNT)) # Start encoding at character 'a'=97
-  CHAR=$(printf "\x$(printf %x $OFFSET)")
-  printf -- "\n--$OFFSET '$CHAR' $name\n" >> "$WIPFILE"
-  printf "poke(0x5600+(8* $OFFSET),\n" >> "$WIPFILE"
-  ROW=1
-  while [ $ROW -le 8 ]; do
-    printf ' %3u' "$((2#`sed -n "$ROW"p "$HBIN" | rev`))" >> "$WIPFILE"
-    if [ $ROW -lt 8 ]; then
-      printf -- ", -- " >> "$WIPFILE"
+  ((ascii_code=96+img_counter)) # Start encoding at character 'a'=97
+  ascii_char=$(printf "\x$(printf %x ${ascii_code})")
+  echo -e "\n--${ascii_code} '${ascii_char}' ${name_lowercase}" >> "${p8_file}"
+  echo "poke(0x5600+(8* ${ascii_code})" >> "${p8_file}"
+  row=1
+  while [ $row -le 8 ]; do
+    printf ' %3u' "$((2#`sed -n ${row}p "$HBIN" | rev`))" >> "${p8_file}"
+    if [ $row -lt 8 ]; then
+      echo -n ', -- ' >> "${p8_file}"
     else
-      printf -- "  -- " >> "$WIPFILE"
+      echo -n '  -- ' >> "${p8_file}"
     fi
-    sed -n "$ROW"p "$HBIN" | tr "0" "▒" | tr "1" "█" >> "$WIPFILE"
-    ((ROW=ROW+1))
+    sed -n ${row}p "$HBIN" | tr '01' '▒█' >> "${p8_file}"
+    ((row=row+1))
   done
-  printf ")\n" >> "$WIPFILE"
+  printf ')\n' >> "${p8_file}"
   # Helper code snippet to copy font character to Sprite 0
-  echo '-->spr0: print"⁶@56000003⁸x⁸⁶c0ᵉ'$CHAR'"for i=0,448,64do memcpy(i,24576+i,4)end cstore()' >> "$WIPFILE"
+  printf -- '-->spr0: print"⁶@56000003⁸x⁸⁶c0ᵉ%s"for i=0,448,64do memcpy(i,24576+i,4)end cstore()\n' "${ascii_char}">> "${p8_file}"
   # Bonus: 'magic' one-off character, encoded as a string
-  COLUMN=8; VALUE=""; DIGIT=0
+  column=8; VALUE=""; digit_present=0
   # We store the end of the string first
   STRING="\""
-  while [ $COLUMN -gt 0 ]; do
+  while [ $column -gt 0 ]; do
     # Find the decimal value for the current column (byte)
-    VALUE=$(printf '%i' "$((2#`sed -n "$COLUMN"p "$HBIN" | rev`))")
-    if [ $VALUE -eq 0 ] && [ $DIGIT -eq 1 ]; then
+    VALUE=$(printf '%i' "$((2#`sed -n ${column}p "$HBIN" | rev`))")
+    if [ $VALUE -eq 0 ] && [ ${digit_present} -eq 1 ]; then
       STRING=`echo "\\000"`$STRING
-      DIGIT=0
+      digit_present=0
     else
       ((VALUE=VALUE+1)) 
-      STRING=`sed -n "$VALUE"p "$UTILDIR/p8-codepage"`$STRING
+      STRING=`sed -n ${VALUE}p "${util_dir}/p8-codepage"`$STRING
       if [ $VALUE -ge 49 ] && [ $VALUE -le 58 ] ; then
-        DIGIT=1
+        digit_present=1
       else
-        DIGIT=0
+        digit_present=0
       fi
     fi
-    ((COLUMN=COLUMN-1))
+    ((column=column-1))
   done
-  echo "--magic: ?\"⁶rw¹シ⁶.\"..\""$STRING >> "$WIPFILE"
+  printf -- '--magic: ?"⁶rw¹シ⁶.".."%s\n' "${STRING}" >> "${p8_file}"
   # Bonus: For 4x4px patterns produce fillp() alternative
-  if [ $PATTERNWIDTH -le 4 ] && [ $PATTERNHEIGHT -le 4 ]; then
-    printf -- "--fillp\"" >> "$WIPFILE"
-    ROW=1; VALUE=""; STRING="0x"
-    while [ $ROW -le 4 ]; do
+  if [ $pattern_width -le 4 ] && [ $pattern_height -le 4 ]; then
+    printf -- '--fillp"' >> "${p8_file}"
+    row=1; VALUE=""; STRING="0x"
+    while [ $row -le 4 ]; do
       # Invert binary values 0<>1 to fix fore-/background
-      VALUE=$(sed -n "${ROW}"p "${HBIN}" | head -c 4 | tr "01" "10")
+      VALUE=$(sed -n ${row}p "${HBIN}" | head -c 4 | tr '01' '10')
       STRING=${STRING}$(printf '%X' $((2#${VALUE})))
-      ((ROW=ROW+1))
+      ((row=row+1))
     done
-    printf '%u\"\n' "$STRING" >> "$WIPFILE"
+    printf '%u\"\n' "$STRING" >> "${p8_file}"
   fi
 
   
   # Create Thumby code
-  WIPFILE="${1%/*/*}/$GROUP.thumby.WIP.txt"
-  printf "\n$nAME = bytearray([\n" >> "$WIPFILE"
-  printf "    # BITMAP: width: 8, height: 8, [" >> "$WIPFILE"
-  COLUMN=1
-  while [ $COLUMN -le 8 ]; do
-    printf '%u' "$((2#`sed -n "$COLUMN"p "$VBIN"`))" >> "$WIPFILE"
-    if [ $COLUMN -le 7 ]; then
-      printf "," >> "$WIPFILE"
+  thumby_file="${img_root}/${img_group}.thumby.WIP.txt"
+  printf '\n$%s = bytearray([\n' "${name_camelcase}" >> "${thumby_file}"
+  printf '    # BITMAP: width: 8, height: 8, [' >> "${thumby_file}"
+  column=1
+  while [ ${column} -le 8 ]; do
+    printf '%u' "$((2#`sed -n ${column}p "$VBIN"`))" >> "${thumby_file}"
+    if [ ${column} -le 7 ]; then
+      printf ',' >> "${thumby_file}"
     else
-      printf "]\n" >> "$WIPFILE"
+      printf ']\n' >> "${thumby_file}"
     fi
-    ((COLUMN=COLUMN+1))
+    ((column=column+1))
   done
-  cat "$VHEX" >> "$WIPFILE"
-  printf "])\n# "$nAME"Sprite = thumby.Sprite(8, 8, "$nAME")\n" >> "$WIPFILE"
-
+  cat "$VHEX" >> "${thumby_file}"
+  printf '])\n# %sSprite = thumby.Sprite(8, 8, %s)\n' "${name_camelcase}" "${name_camelcase}" >> "${thumby_file}"
 
   # Remove temporary files
   rm "$HBIN" ; rm "$VBIN" ; rm "$HHEX" ; rm "$VHEX"
