877fe4979940bd9bb249682606e3644829a07857
Andrew Dent <dent.ace@gmail.com>
2023-05-28 12:38:04 +0100

    Add code snippets for Other patterns
    - Add code for `Other`.
    - Update links in group’s README.

--- 15-Other/Other.thumby.py
+++ 15-Other/Other.thumby.py
@@ -2,3 +2,28 @@
 # This work is dedicated to the Public Domain by ACED, licensed under CC0
 # https://creativecommons.org/publicdomain/zero/1.0/
 
+chain = bytearray([
+    # BITMAP: width: 8, height: 8, [0,0,102,153,102,0,0,0]
+    0x00,  # ▓▓▓▓▓▓▓▓
+    0x00,  # ▓▓▓▓▓▓▓▓
+    0x66,  # ▓░░▓▓░░▓
+    0x99,  # ░▓▓░░▓▓░
+    0x66,  # ▓░░▓▓░░▓
+    0x00,  # ▓▓▓▓▓▓▓▓
+    0x00,  # ▓▓▓▓▓▓▓▓
+    0x00,  # ▓▓▓▓▓▓▓▓
+])
+# chainSprite = thumby.Sprite(8, 8, chain)
+
+chainLarge = bytearray([
+    # BITMAP: width: 8, height: 8, [0,60,66,231,66,60,0,0]
+    0x00,  # ▓▓▓▓▓▓▓▓
+    0x3C,  # ▓▓░░░░▓▓
+    0x42,  # ▓░▓▓▓▓░▓
+    0xE7,  # ░░░▓▓░░░
+    0x42,  # ▓░▓▓▓▓░▓
+    0x3C,  # ▓▓░░░░▓▓
+    0x00,  # ▓▓▓▓▓▓▓▓
+    0x00,  # ▓▓▓▓▓▓▓▓
+])
+# chainLargeSprite = thumby.Sprite(8, 8, chainLarge)
