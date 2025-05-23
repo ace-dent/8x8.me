941f1cd95b5035bb8cbd64b8ea5922901f9a51d1
Andrew Dent <dent.ace@gmail.com>
2025-05-23 22:05:31 +0100

    Dots: Add SPDX headers
    - Update code snippets with SPDX headers.

--- 02-Dots/Dots.h
+++ 02-Dots/Dots.h
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: CC0-1.0
+
 // Dots - 8x8.me fill patterns
 // This work is dedicated to the Public Domain by ACED, licensed under CC0
 // https://creativecommons.org/publicdomain/zero/1.0/
